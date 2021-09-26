05f5bf41b9d74bf3a3424caef5298258800a9a1f
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-09-26 12:49:09 +0200

    Update AES.py

--- Metrics/AES.py
+++ Metrics/AES.py
@@ -12,17 +12,52 @@ img = nib.load(img_dir + "brainmask.mgz")
 img = np.asarray(img.dataobj)
 img = img.astype(float)
 
-slice = 90
 
+def aes(img):
+    '''
+    Parameters
+    ----------
+    img : numpy array
+        image for which the metrics should be calculated.
 
-
-x_kern = np.array([[-1,-1,-1],
+    Returns
+    -------
+    AES : float
+        Average Edge Strength measure of the input image.
+    '''
+    x_kern = np.array([[-1,-1,-1],
                    [0,0,0],
                    [1,1,1]])
 
-y_kern = x_kern.T
+    y_kern = x_kern.T
 
-#Convolve slice
-x_conv = convolve(img[slice,:,:], x_kern)
-y_conv = convolve(img[slice,:,:], y_kern)
+    #shape of volume/img
+    vol_shape = np.shape(img)
 
+    #Empty array to contain edge strenghts
+    es = []
+
+    #for each slice calcule the edge strength
+    for slice in range(vol_shape[0]):
+
+
+        #Convolve slice
+        x_conv = convolve(img[slice,:,:], x_kern)
+        y_conv = convolve(img[slice,:,:], y_kern)
+        canny_img = canny(img[slice,:,:])
+
+        numerator = np.sum(canny_img*( x_conv**2 + y_conv**2 ))
+        denominator = np.sum(canny_img)
+
+        #note different result if we divide by zero then take nanmean
+        #compared to replacing frac value by zero then nanmean
+        #if we dont check we can remove if statement and just use 
+        #np.nanmean
+        if denominator>0:
+            frac = np.sqrt(numerator)/denominator
+        else: frac = 0
+        es.append(frac)
+
+    return np.nanmean(es)
+
+print(aes(img))

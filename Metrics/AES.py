afe382b1a2303124e12e4840620a11c7036d0832
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-09-26 12:53:31 +0200

    Update AES.py

--- Metrics/AES.py
+++ Metrics/AES.py
@@ -25,27 +25,29 @@ def aes(img):
     AES : float
         Average Edge Strength measure of the input image.
     '''
+    #Centered Gradient kernel in the x-direction
     x_kern = np.array([[-1,-1,-1],
                    [0,0,0],
                    [1,1,1]])
-
+    #Centered Gradient kernel in the y-direction
     y_kern = x_kern.T
 
     #shape of volume/img
     vol_shape = np.shape(img)
 
     #Empty array to contain edge strenghts
+    #Function returns the mean of this list
     es = []
 
     #for each slice calcule the edge strength
     for slice in range(vol_shape[0]):
-
-
         #Convolve slice
         x_conv = convolve(img[slice,:,:], x_kern)
         y_conv = convolve(img[slice,:,:], y_kern)
+        #Canne edge detector
         canny_img = canny(img[slice,:,:])
-
+        #Numerator and denominator, to be divided
+        #defining the edge strength of the slice
         numerator = np.sum(canny_img*( x_conv**2 + y_conv**2 ))
         denominator = np.sum(canny_img)
 
@@ -56,8 +58,9 @@ def aes(img):
         if denominator>0:
             frac = np.sqrt(numerator)/denominator
         else: frac = 0
+        #Append the edge strength
         es.append(frac)
-
+    #Return the average edge strength
     return np.nanmean(es)
 
 print(aes(img))

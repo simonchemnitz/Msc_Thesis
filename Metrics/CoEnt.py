d34db1669dc91d9cd548544f68ae6fa339128087
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-09-26 11:15:05 +0200

    Update CoEnt.py

--- Metrics/CoEnt.py
+++ Metrics/CoEnt.py
@@ -9,4 +9,15 @@ def coent(img, levels = 256):
 
     co_ent_matrix = np.zeros((levels,levels))
 
-    
+    for i in range(vol_shape[0]):
+        tmp_comat = greycomatrix(img[i,:,:],
+                                 levels = levels,
+                                 distance = [1],
+                                 angles = [np.pi,-np.pi, 
+                                           np.pi/2,-np.pi/2])
+        tmp_comat = np.sum(tmp_comat[:,:,0,:], axis = 2)
+        
+        co_ent_matrix = co_ent_matrix+tmp_comat
+    for j in range(vol_shape[1]):
+        tmp_comat = greycomatrix(img[:,j,:],
+        levels = levels, distance = [1])

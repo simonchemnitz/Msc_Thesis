3ccd36511b55933b3e4e28cf52db9bca41f4afdb
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-09-26 12:06:55 +0200

    Update AES.py

--- Metrics/AES.py
+++ Metrics/AES.py
@@ -1,7 +1,7 @@
 import numpy as np
 import nibabel as nib
 import glob
-
+from skimage.feature import canny
 
 #load img
 img_dir = "C:/Users/simon/OneDrive/Skrivebord/UNI/MASTER_THESIS/recon/subj04/mri"
@@ -10,3 +10,12 @@ img = np.asarray(img.dataobj)
 img = img.astype(float)
 
 
+
+x_kern = np.array([[-1,-1,-1],
+                   [0,0,0],
+                   [1,1,1]])
+
+y_kern = x_kern.T
+
+print(x_kern)
+print(y_kern)

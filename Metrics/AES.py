be1dfcca4e2f8840485fe76cca65fe817cd9909b
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-09-26 12:16:31 +0200

    Update AES.py

--- Metrics/AES.py
+++ Metrics/AES.py
@@ -2,12 +2,17 @@ import numpy as np
 import nibabel as nib
 import glob
 from skimage.feature import canny
+from scipy.ndimage import convolve
+import matplotlib.pyplot as plt
+
 
 #load img
-img_dir = "C:/Users/simon/OneDrive/Skrivebord/UNI/MASTER_THESIS/recon/subj04/mri"
+img_dir = "C:/Users/simon/OneDrive/Skrivebord/UNI/MASTER_THESIS/recon/subj04/mri/"
 img = nib.load(img_dir + "brainmask.mgz")
 img = np.asarray(img.dataobj)
 img = img.astype(float)
+
+slice = 90
 
 
 
@@ -17,5 +22,7 @@ x_kern = np.array([[-1,-1,-1],
 
 y_kern = x_kern.T
 
-print(x_kern)
-print(y_kern)
+#Convolve slice
+x_conv = convolve(img[slice,:,:], x_kern)
+y_conv = convolve(img[slice,:,:], y_kern)
+

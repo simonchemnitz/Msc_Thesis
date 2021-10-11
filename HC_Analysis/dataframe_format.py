1435b569a89324b218770f7944429f0becc6b361
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-10-11 23:36:51 +0200

    Update dataframe_format.py

--- HC_Analysis/dataframe_format.py
+++ HC_Analysis/dataframe_format.py
@@ -37,4 +37,28 @@ subjects = glob.glob(metric_in+"*")
 #for each subject create a dataframe
 for sub in subjects:
     subject = sub[len(metric_in):]
-    base_csv.to_csv(hc_out+subject+".csv", index = False)
+    base_csv.to_csv(metric_out+subject+".csv", index = False)
+
+
+#Format subject metric-DataFrames
+#Folders for each subject
+subjects = glob.glob(metric_in+"*")
+#Format all metric files
+for subject in subjects:
+    for file in glob.glob(subject+"/*"):
+        format_dataframe_metric(file)
+
+
+#Merge all metric scores
+#DataFrame template
+merged_df = pd.DataFrame(columns = ['aes', 'coent', 'moco', 'nod', 'RR', 
+                                   'shake', 'still', 'pers_id', 'img_type'])
+
+
+for file in glob.glob(hc_out+"*"):
+    if "merged" not in file:
+        #load file 
+        file_df = pd.read_csv(file)
+        merged_df = pd.concat([merged_df, file_df])
+
+merged_df.to_csv(hc_out+"merged_metric.csv", index = False)

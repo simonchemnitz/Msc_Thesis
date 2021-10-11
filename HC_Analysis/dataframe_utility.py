adeffa7c5d2a8398acee1253ddd3547ec671a5cd
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-10-11 23:15:39 +0200

    Update dataframe_utility.py

--- HC_Analysis/dataframe_utility.py
+++ HC_Analysis/dataframe_utility.py
@@ -3,3 +3,21 @@ import pandas as pd
 import glob 
 import os
 
+
+#File directories
+
+#metric directories
+mainhc_in = "Results/"
+hc_out = "HC_metric_csv/"
+#observer directories
+observer_in = "observer_scores/"
+observer_out = "HC_obs_csv/"
+
+#date of the lates evalution
+relevant_date = "10_06"
+#filename reference
+file_reference = "Values_AES_CoEnt_10_06_"
+
+
+
+

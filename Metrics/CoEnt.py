2722fef5c10ea728e5272483ff693bf99801b4ea
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-10-12 15:55:45 +0200

    added coent for 2d encoded

--- Metrics/CoEnt.py
+++ Metrics/CoEnt.py
@@ -7,7 +7,7 @@ import numpy as np
 from skimage.feature import greycomatrix
 from img_utils import bin_img, crop_img
 
-def coent(img, brainmask = None, n_levels = 128, bin = True, crop = True, supress_zero = True):
+def coent3d(img, brainmask = None, n_levels = 128, bin = True, crop = True, supress_zero = True):
     '''
     Parameters
     ----------
@@ -96,3 +96,96 @@ def coent(img, brainmask = None, n_levels = 128, bin = True, crop = True, supres
     log_matrix = np.log2(co_ent_matrix)
     #Return the entropy
     return -np.nansum(co_ent_matrix*log_matrix)
+
+
+
+
+
+
+def coent2d(img, brainmask = None, n_levels = 128, bin = True, crop = True, supress_zero = True):
+    '''
+    Parameters
+    ----------
+    img : numpy array
+        Image for which the metrics should be calculated.
+    n_levels : int
+        Levels of intensities to bin image by
+    bin : bool
+        Whether or not to bin the image
+    crop : bool 
+        Whether or not to crop image/ delete empty slices 
+    Returns
+    -------
+    CoEnt : float
+        Co-Occurrence Entropy measure of the input image.
+    '''
+    #Apply brainmask if given one
+    if brainmask is not None: #alternative type(brainmask) != type(None)
+        img = img*brainmask
+    #Crop image if crop is True
+    if crop:
+        img = crop_img(img)
+    #Bin image if bin is True
+    if bin:
+        img = bin_img(img, n_levels=n_levels)
+    #Scale imgage to have intensity values in [0,255]
+    img = 255*(img/np.max(img))
+    #Convert image to uint8
+    #   as greycomatrix prefers uint8 as input
+    img = img.astype(np.uint8)
+
+    #Shape of the image/volume
+    vol_shape = np.shape(img)
+
+    comat = np.zeros((256,256))
+
+
+    #Assuming volume is of the format
+    # V(i,j,k) where k denotes the slice number
+    for slice in range(vol_shape[2]):
+        #Temporary co-occurrence matrix
+        tmp_comat = greycomatrix(img[:,:,slice],
+                                 distances = [1],
+                                 angles = [0*(np.pi/2),
+                                           1*(np.pi/2),
+                                           2*(np.pi/2),
+                                           3*(np.pi/2)])
+        tmp_comat = np.sum(tmp_comat[:,:,0,:], axis = 2)
+        comat = comat + tmp_comat
+    
+    #normallise comat
+    comat =  comat/np.sum(comat)
+
+    if supress_zero:
+        comat[0,0] = 0
+
+    #Take log2 to get entropy
+    log_matrix = np.log2(comat)
+    #Return the entropy
+    return -np.nansum(comat*log_matrix)        
+
+
+def coent(img, brainmask = None, n_levels = 128, bin = True, crop = True, supress_zero = True):
+    '''
+    Parameters
+    ----------
+    img : numpy array
+        Image for which the metrics should be calculated.
+    n_levels : int
+        Levels of intensities to bin image by
+    bin : bool
+        Whether or not to bin the image
+    crop : bool 
+        Whether or not to crop image/ delete empty slices 
+    Returns
+    -------
+    CoEnt : float
+        Co-Occurrence Entropy measure of the input image.
+    '''
+
+    #check which function to use:
+    img_vol = np.shape(img)
+
+    if img_vol[2]<100:
+        return coent2d(img, brainmask, n_levels, bin, crop, supress_zero)
+    else: return coent3d(img, brainmask, n_levels, bin, crop, supress_zero)

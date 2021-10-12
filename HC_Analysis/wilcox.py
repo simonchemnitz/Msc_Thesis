11a4308d01c41db235a4b1d2b288f6b28b2a5873
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-10-12 17:39:41 +0200

    Update wilcox.py

--- HC_Analysis/wilcox.py
+++ HC_Analysis/wilcox.py
@@ -14,7 +14,7 @@ df = pd.read_csv(main_dir + "observer_merged_metric.csv")
 
 
 
-def wilcox_test(df, nod, RR,shake, metric):
+def wilcox_test(df, nod, RR,shake, img_type, metric):
 
     #create a copy of the DataFrame
     cox_df = df.copy()
@@ -22,19 +22,28 @@ def wilcox_test(df, nod, RR,shake, metric):
     cox_df = cox_df.loc[cox_df["nod"] == nod]
     cox_df = cox_df.loc[cox_df["RR"] == RR]
     cox_df = cox_df.loc[cox_df["shake"] == shake]
+    cox_df = cox_df.loc[cox_df["img_type"] == img_type]
 
     #Drop redundant columns
-    cox_df = cox_df.drop(["nod", "still", "RR", "shake"], axis = 1)
+    cox_df = cox_df.drop(["nod", "still", "RR", "shake", "img_type"], axis = 1)
 
     #Subset for only relevant columnds
-    cox_df = cox_df[["pers_id", "img_type", "moco", metric]]
+    cox_df = cox_df[["pers_id", "moco", metric]]
 
-    cox_df = cox_df.set_index(['pers_id', 'img_type', 'moco'])[metric].unstack().reset_index()
-    #print(cox_df)
+    cox_df = cox_df.set_index(['pers_id', 'moco'])[metric].unstack().reset_index()
 
+    #calculate the cox
     print( cox( x = cox_df[0] , y = cox_df[1] ) )
 
 
 
 
-wilcox_test(df, nod = 1, RR = 0,shake = 0, metric = "aes")
+img_types = df["img_type"].unique()
+
+print("Wilcoxon rank test")
+print("Nod")
+for img_type in img_types:
+    print()
+    print(img_type)
+    print("Wilcoxon rank test:")
+    wilcox_test(df, nod = 1, RR = 0,shake = 0, img_type = img_type ,metric = "coent")

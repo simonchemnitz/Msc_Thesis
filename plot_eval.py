42b6303596ecb3025a495e81760764a1dee7f730
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-10-18 22:21:07 +0200

    Update plot_eval.py

--- plot_eval.py
+++ plot_eval.py
@@ -4,39 +4,26 @@ import glob
 from plot_utility import correlation_plot, starbox_plot
 import matplotlib.pyplot as plt
 data_dir = "C:/Users/simon/Documents/GitHub/Msc_Thesis/HC_Analysis/Files_ig"
-print("LOADING DATA")
+
 df = pd.read_csv(data_dir + "/observer_merged_metric.csv")
 
 
-
-#cor = correlation_plot(df, img_seq= "T1_MPR_", title = "Plot Title", x = "w_avg", y = "coent", fit_line = True,
-#                            x_label= "Observer Scores", y_label="CoEnt")
-
-
-#print(type(cor))
-#plt.show()
+for img_type in df["img_type"].unique():
+    for metric in ["coent", "aes"]:
+        cor = correlation_plot(df, img_seq= img_type, title = "Plot Title", x = "w_avg", y = metric, fit_line = True,
+                          x_label= "Observer Scores", y_label=metric)
+    plt.show()
 
 
 
-#for img_type in df["img_type"].unique():
-#   cor = correlation_plot(df, img_seq= img_type, title = "Plot Title", x = "w_avg", y = "aes", fit_line = True,
-#                          x_label= "Observer Scores", y_label="CoEnt")
 
-
-
-print("LOADING COX DF")
 cox_dir = "C:/Users/simon/Documents/GitHub/Msc_Thesis/HC_Analysis/"
 
 cox_df = pd.read_csv(cox_dir + "wilcox_results.csv")
 
 
-print("done")
 
 
-##bp = starbox_plot(df, "T1_MPR", "pers_id", "moco", "aes", "plot titel", nod = 0, wilcox_df= cox_df)
-
-
-#plt.show()
 
 
 for img_type in df["img_type"].unique():

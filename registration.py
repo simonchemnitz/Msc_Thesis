d65a1c88f7c027485c9fc2d2e6e5d67b752fd19e
simonchemnitz <55578391+simonchemnitz@users.noreply.github.com>
2021-10-26 11:22:33 +0200

    Update registration.py

--- registration.py
+++ registration.py
@@ -3,7 +3,7 @@ import os
 import glob
 
 
-def mri_reg(sub, nifti_dir, output_dir):
+def bbregistration(sub, nifti_dir, output_dir):
     """
     Parameters
     ----------
@@ -15,7 +15,8 @@ def mri_reg(sub, nifti_dir, output_dir):
         Filepath where output should be saved
     """
 
-    registration_directory = output_dir + "regs/"+sub
+    registration_directory = output_dir + "Registration/"+sub+"/regs/"
+    print(registration_directory)
     #Check if the registration_directory exists
     #Create folder if it does not
     if not os.path.exists(registration_directory):
@@ -35,12 +36,12 @@ def mri_reg(sub, nifti_dir, output_dir):
             seq_name = os.path.basename(nifti)[:-4]
             #Output file name
             output_name = seq_name+".lta"
-            regname = output_name
+            regname = registration_directory+"/"+output_name
 
             #Perform bb registration
-            print('bbregister --s ' + sub + ' --mov '+  movImg + ' --reg ' + regname + ' --t2 --init-best-header')
+            #print('bbregister --s ' + sub + ' --mov '+  movImg + ' --reg ' + regname + ' --t2 --init-best-header')
             #subprocess.run('bbregister --s ' + sub + ' --mov '+  movImg + ' --reg ' + regname + ' --t2 --init-best-header', shell=True)
-            
+
             #Print progress
             print()
             print(str(i)+"/"+str(len(nifti_to_reg)), "Done")
@@ -52,13 +53,37 @@ def mri_reg(sub, nifti_dir, output_dir):
     print("+------------------------------------------------------------------+")
 
 
+def apply_registration(sub, recon_dir, nifti_dir, output_dir):
+    """
+    Parameters
+    ----------
+    sub : str
+        subject name as stated in the freesurfer SUBJECT_DIR
+    nifti_dir : str
+        Filepath for the nifti of the subject
+    output_dir : str
+        Filepath where output should be saved
+    """
+    #Binarize brainmask
+    #Path to brainmask
+    brainmask = recon_dir+sub+"/mri/brainmask.mgz"
+    print(brainmask)
+    #Output name
+    binary_brainmask_nii = output_dir+sub+"/bin_brainmask.nii"
+    print(binary_brainmask_nii)
+    #subprocess.run('mri_binarize --i ' + brainmask + ' --o ' + binary_brainmask_nii + ' --match 0 --inv', shell=True)
 
 
-nifti_dir = "/users/simon/desktop/data1/MRI_scans/nifti/"
-output_dir = "/users/simon/desktop/data1/MRI_SCANS/"
+    return None
+
+nifti_dir = "/users/simon/desktop/data1/Chemnitz-Thomsen_Simon/MRI_scans/nifti/"
+output_dir = "/users/simon/desktop/data1/Chemnitz-Thomsen_Simon/MRI_SCANS/"
+recon_dir = "/users/simon/desktop/data1/Chemnitz-Thomsen_Simon/MRI_SCANS/fs_test_simon/"
 
 subject = "MOCO_001"
 
 
-mri_reg(subject, nifti_dir, output_dir)
+bbregistration(subject, nifti_dir, output_dir)
     
+
+apply_registration(subject, recon_dir, nifti_dir, output_dir)
